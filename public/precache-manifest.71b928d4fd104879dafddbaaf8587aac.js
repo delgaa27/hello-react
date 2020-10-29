1bb92880755d31dc39601f7705ec8158c156a445
delgaa27 <67293615+delgaa27@users.noreply.github.com>
2020-10-29 13:41:38 -0300

    Add files via upload

---
+++ public/precache-manifest.71b928d4fd104879dafddbaaf8587aac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59a077df9b8eaa4acb989ad41f3e15bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "523b68bdea225c8f3b83",
+    "url": "/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "635e33a91079d1b1f01b",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "523b68bdea225c8f3b83",
+    "url": "/static/js/2.534d935e.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.534d935e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "635e33a91079d1b1f01b",
+    "url": "/static/js/main.6bf809fd.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
